ff6069bdb36b497fc26a16887210b9f09448f382
zainammar <zainammar74@gmail.com>
2026-05-16 15:10:19 +0500

    Deploment Issues

--- themeOne/urls.py
+++ themeOne/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # Home page (static slug)
+    path('', views.page_detail, {'slug': 'home'}, name='home'),
+
+    # Dynamic pages
+    path('<slug:slug>/', views.page_detail, name='page_detail'),
 ]
