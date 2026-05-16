b15b7ec3f1c9d65137f1d20538ea5331039cc61a
zainammar <zainammar74@gmail.com>
2026-05-16 14:57:17 +0500

    Fix urls routing for deployment

--- themeOne/urls.py
+++ themeOne/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Home page (static slug)
-    path('', views.page_detail, {'slug': 'home'}, name='home'),
-
-    # Dynamic pages
-    path('<slug:slug>/', views.page_detail, name='page_detail'),
+    path('', views.home, name='home'),
 ]
--- webeasy/urls.py
+++ webeasy/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,11 +7,3 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('', include('themeOne.urls')),
 ]
-
-
-# Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
